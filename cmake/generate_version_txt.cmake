8760b40e0c6e71595a3920c7dac88b03ecde6dbb
mean <fixounet@free.fr>
2023-11-06 19:58:15 +0100

    update to -17.0.1 for rv32

--- cmake/generate_version_txt.cmake
+++ cmake/generate_version_txt.cmake
@@ -6,29 +6,30 @@
 # archive was created. This is configured in the .gitattributes file.
 # In the former case, this script will run a Git command to find out the
 # current revision. In the latter case the revision will be used as is.
-set(LLVMEmbeddedToolchainForArm_COMMIT "$Format:%H$")
+#FX set(LLVMEmbeddedToolchainForArm_COMMIT "$Format:%H$")
+set(LLVMEmbeddedToolchainForArm_COMMIT "None")
 
-if(NOT ${LLVMEmbeddedToolchainForArm_COMMIT} MATCHES "^[a-f0-9]+$")
-    execute_process(
-        COMMAND git -C ${LLVMEmbeddedToolchainForArm_SOURCE_DIR} rev-parse HEAD
-        OUTPUT_VARIABLE LLVMEmbeddedToolchainForArm_COMMIT
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        COMMAND_ERROR_IS_FATAL ANY
-    )
-endif()
+#if(NOT ${LLVMEmbeddedToolchainForArm_COMMIT} MATCHES "^[a-f0-9]+$")
+#execute_process(
+#COMMAND git -C ${LLVMEmbeddedToolchainForArm_SOURCE_DIR} rev-parse HEAD
+#OUTPUT_VARIABLE LLVMEmbeddedToolchainForArm_COMMIT
+#OUTPUT_STRIP_TRAILING_WHITESPACE
+#COMMAND_ERROR_IS_FATAL ANY
+#    )
+#endif()
 
-execute_process(
-    COMMAND git -C ${llvmproject_SOURCE_DIR} rev-parse HEAD
-    OUTPUT_VARIABLE llvmproject_COMMIT
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    COMMAND_ERROR_IS_FATAL ANY
-)
-execute_process(
-    COMMAND git -C ${picolibc_SOURCE_DIR} rev-parse HEAD
-    OUTPUT_VARIABLE picolibc_COMMIT
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    COMMAND_ERROR_IS_FATAL ANY
-)
+#execute_process(
+#COMMAND git -C ${llvmproject_SOURCE_DIR} rev-parse HEAD
+#OUTPUT_VARIABLE llvmproject_COMMIT
+#OUTPUT_STRIP_TRAILING_WHITESPACE
+#COMMAND_ERROR_IS_FATAL ANY
+#)
+#execute_process(
+#COMMAND git -C ${picolibc_SOURCE_DIR} rev-parse HEAD
+#OUTPUT_VARIABLE picolibc_COMMIT
+#OUTPUT_STRIP_TRAILING_WHITESPACE
+#COMMAND_ERROR_IS_FATAL ANY
+#)
 
 configure_file(
     ${CMAKE_CURRENT_LIST_DIR}/VERSION.txt.in
